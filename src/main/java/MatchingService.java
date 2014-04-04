a4caa2d69c178a150fa03b46759a59409010c061
sumithra gomadam <sumithra.gomadam@bazaarvoice.com>
2014-04-04 10:33:38 -0500

    latest working

--- src/main/java/MatchingService.java
+++ src/main/java/MatchingService.java
@@ -21,41 +21,41 @@ public class MatchingService extends Service<MatchingServiceConfig>{
 
         jedis.sadd("bestbuy.users", "1", "2", "3", "4", "5");
 
-        jedis.sadd("bestbuy.users.1.1", "1", "2", "3", "12");
+        jedis.sadd("bestbuy.users.1.1", "test1", "2", "mi80100229", "999977000050005");
         //jedis.sadd("users.1.2", null);
-        jedis.sadd("bestbuy.users.1.3", "4", "5");
-        jedis.sadd("bestbuy.users.1.4", "6", "7");
-        jedis.sadd("bestbuy.users.1.5", "8", "9");
+        jedis.sadd("bestbuy.users.1.3", "abcat0105000", "999997300050001");
+        jedis.sadd("bestbuy.users.1.4", "9999941", "999977100050007");
+        jedis.sadd("bestbuy.users.1.5", "999977100050005", "999977100050002");
 
-        jedis.sadd("bestbuy.users.2.1", "1", "2");
-        jedis.sadd("bestbuy.users.2.2", "4");
-        jedis.sadd("bestbuy.users.2.3", "3", "5");
-        jedis.sadd("bestbuy.users.2.4", "9", "10");
-        jedis.sadd("bestbuy.users.2.5", "7", "8");
+        jedis.sadd("bestbuy.users.2.1", "test1", "test");
+        jedis.sadd("bestbuy.users.2.2", "abcat0105000");
+        jedis.sadd("bestbuy.users.2.3", "mi80100229", "999997300050001");
+        jedis.sadd("bestbuy.users.2.4", "999977100050002", "999977100050001");
+        jedis.sadd("bestbuy.users.2.5", "999977100050007", "999977100050005");
 
         //jedis.sadd("users.3.1", null);
         //jedis.sadd("users.3.2", null);
-        jedis.sadd("bestbuy.users.3.3", "1", "2");
-        jedis.sadd("bestbuy.users.3.4", "8", "9");
+        jedis.sadd("bestbuy.users.3.3", "test1", "test");
+        jedis.sadd("bestbuy.users.3.4", "999977100050005", "999977100050002");
         //jedis.sadd("users.3.5", null);
 
-        jedis.sadd("bestbuy.users.4.1", "1", "2", "3", "11");
+        jedis.sadd("bestbuy.users.4.1", "test1", "test", "mi80100229", "999977100050000");
         //jedis.sadd("users.4.2", null);
-        jedis.sadd("bestbuy.users.4.3", "4", "5");
-        jedis.sadd("bestbuy.users.4.4", "6", "7");
-        jedis.sadd("bestbuy.users.4.5", "8", "9", "13");
+        jedis.sadd("bestbuy.users.4.3", "abcat0105000", "999997300050001");
+        jedis.sadd("bestbuy.users.4.4", "9999941", "999977100050007");
+        jedis.sadd("bestbuy.users.4.5", "999977100050005", "999977100050002", "999977000050004");
 
         //jedis.sadd("users.5.1", null);
         //jedis.sadd("users.5.2", null);
-        jedis.sadd("bestbuy.users.5.3", "1", "2");
-        jedis.sadd("bestbuy.users.5.4", "8", "9");
+        jedis.sadd("bestbuy.users.5.3", "test1", "test");
+        jedis.sadd("bestbuy.users.5.4", "999977100050005", "999977100050002");
         //jedis.sadd("users.5.5", null);
 
-        jedis.sadd("bestbuy.inter.1-2.1", "1", "2");
+        jedis.sadd("bestbuy.inter.1-2.1", "test1", "test");
         //jedis.sadd("inter.1-2.2", null);
-        jedis.sadd("bestbuy.inter.1-2.3", "5");
+        jedis.sadd("bestbuy.inter.1-2.3", "999997300050001");
         //jedis.sadd("inter.1-2.4", null);
-        jedis.sadd("bestbuy.inter.1-2.5", "8");
+        jedis.sadd("bestbuy.inter.1-2.5", "999977100050005");
 
         /*jedis.sadd("inter.1-3.1", null);
         jedis.sadd("inter.1-3.2", null);
@@ -63,12 +63,12 @@ public class MatchingService extends Service<MatchingServiceConfig>{
         jedis.sadd("inter.1-3.4", null);
         jedis.sadd("inter.1-3.5", null);*/
 
-        jedis.sadd("bestbuy.inter.1-4.1", "1", "2", "3");
+        jedis.sadd("bestbuy.inter.1-4.1", "test1", "test", "mi80100229");
         //jedis.sadd("inter.1-4.2", null);
 
-        jedis.sadd("bestbuy.inter.1-4.3", "4", "5");
-        jedis.sadd("bestbuy.inter.1-4.4", "6", "7");
-        jedis.sadd("bestbuy.inter.1-4.5", "8", "9");
+        jedis.sadd("bestbuy.inter.1-4.3", "abcat0105000", "999997300050001");
+        jedis.sadd("bestbuy.inter.1-4.4", "9999941", "999977100050007");
+        jedis.sadd("bestbuy.inter.1-4.5", "999977100050005", "999977100050002");
 
         /*jedis.sadd("inter.1-5.1", null);
         jedis.sadd("inter.1-5.2", null);
@@ -79,19 +79,19 @@ public class MatchingService extends Service<MatchingServiceConfig>{
         jedis.sadd("inter.2-3.1", null);
         jedis.sadd("inter.2-3.2", null);
         jedis.sadd("inter.2-3.3", null);*/
-        jedis.sadd("bestbuy.inter.2-3.4", "9");
+        jedis.sadd("bestbuy.inter.2-3.4", "999977100050002");
         //jedis.sadd("inter.2-3.5", null);
 
-        jedis.sadd("bestbuy.inter.2-4.1", "1", "2");
+        jedis.sadd("bestbuy.inter.2-4.1", "test1", "test");
         //jedis.sadd("inter.2-4.2", null);
-        jedis.sadd("bestbuy.inter.2-4.3", "5");
+        jedis.sadd("bestbuy.inter.2-4.3", "999997300050001");
         //jedis.sadd("inter.2-4.4", null);
         //jedis.sadd("inter.2-4.5", null);
 
         //jedis.sadd("inter.2-5.1", null);
         //jedis.sadd("inter.2-5.2", null);
         //jedis.sadd("inter.2-5.3", null);
-        jedis.sadd("bestbuy.inter.2-5.4", "9");
+        jedis.sadd("bestbuy.inter.2-5.4", "999977100050002");
         //jedis.sadd("inter.2-5.5", null);
 
         /*jedis.sadd("inter.3-4.1", null);
@@ -102,8 +102,8 @@ public class MatchingService extends Service<MatchingServiceConfig>{
 
         jedis.sadd("inter.3-5.1", null);
         jedis.sadd("inter.3-5.2", null);*/
-        jedis.sadd("bestbuy.inter.3-5.3", "1", "2");
-        jedis.sadd("bestbuy.inter.3-5.4", "8", "9");
+        jedis.sadd("bestbuy.inter.3-5.3", "test1", "test");
+        jedis.sadd("bestbuy.inter.3-5.4", "999977100050005", "999977100050002");
         /*jedis.sadd("inter.3-5.5", null);
 
         jedis.sadd("inter.4-5.1", null);
